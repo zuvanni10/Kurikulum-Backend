7f9a554b8ad58fdd3846d3545429af75fa562751
zuvanni10 <zuvannii@gmail.com>
2020-09-11 11:12:05 +0700

    Tugas

---
+++ Sprint-1/Topik-5/5.1 Function/latihan-2.php
@@ -0,0 +1,49 @@
+<?php
+
+echo '==================================================='."\n";
+echo '||                                               ||'."\n";
+echo '||                 Luas Bangun                   ||'."\n";
+echo '||                                               ||'."\n";
+echo '==================================================='."\n";
+echo '1. Segitiga'."\n".'2. Lingkaran'."\n".'3. Trapesium'."\n";
+
+$pilihan = (int) trim(fgets(STDIN));
+
+function Segitiga($alas, $tinggi) {
+    echo "Luas Segitiga : ";
+    $luas = ($alas*$tinggi)*1/2;
+    echo $luas . "\n";
+}
+
+function Lingkaran($r) {
+    $phi = 3.14;
+    echo "Luas Lingkaran : ";
+    $luas = $phi*$r**2;
+    echo $luas . "\n";
+}
+
+function Trapesium($y, $z, $t) {
+    echo "Luas Trapesium : ";
+    $luas = 1/2*($y+$z)*$t;
+    echo $luas . "\n";
+}
+
+if ($pilihan == 1) {
+    echo "Alas : ";
+    $alas = (int) trim(fgets(STDIN));
+    echo "Tinggi : ";
+    $tinggi = (int) trim(fgets(STDIN));
+    Segitiga($alas, $tinggi)."\n";
+} else if($pilihan == 2) {
+    echo "Jari-jari : ";
+    $r = (int) trim(fgets(STDIN));
+    Lingkaran($r)."\n";
+} else if($pilihan == 3) {
+    echo "Sisi Sejajar 1 : ";
+    $y = (int) trim(fgets(STDIN));
+    echo "Sisi Sejajar 2 : ";
+    $z = (int) trim(fgets(STDIN));
+    echo "Tinggi : ";
+    $t = (int) trim(fgets(STDIN));
+    Trapesium($y, $z, $t)."\n";
+}
