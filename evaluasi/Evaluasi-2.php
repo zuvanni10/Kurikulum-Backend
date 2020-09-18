2e540b560cdcb1e7e90f4cc84f658551de819ca3
zuvanni10 <zuvannii@gmail.com>
2020-09-18 14:59:11 +0700

    Evaluasi

---
+++ evaluasi/Evaluasi-2.php
@@ -0,0 +1,21 @@
+<?php
+
+class Register {
+    private $student = [
+        [
+            'name' => "Arief",
+            'nik' => "T001"
+        ]
+    ];
+
+    public static function insertStudent($santri): void {
+        
+    }
+
+    public function __constructor() {
+        $this->student = $student;
+    }
+
+}
+
+Register::insertStudent(['name' => $nama, 'nik' => $nik]);
