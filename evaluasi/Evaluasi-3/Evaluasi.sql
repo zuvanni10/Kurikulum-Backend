568827abc4961cc7d77f45483c3d4ca2eb9441f1
zuvanni10 <zuvannii@gmail.com>
2020-09-25 14:47:52 +0700

    Evaluasi

---
+++ evaluasi/Evaluasi-3/Evaluasi.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
+--
+-- Host: localhost    Database: Evaluasi3
+-- ------------------------------------------------------
+-- Server version	5.7.31-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Data`
+--
+
+DROP TABLE IF EXISTS `Data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(30) NOT NULL,
+  `nilai` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Data`
+--
+
+LOCK TABLES `Data` WRITE;
+/*!40000 ALTER TABLE `Data` DISABLE KEYS */;
+INSERT INTO `Data` VALUES (1,'Hadi',90),(2,'Budi',60),(3,'Susi',75),(4,'Umar',55),(5,'Umair',85),(6,'Hasbi',70),(7,'Bayu',95),(8,'Bambang',50),(9,'Aris',100),(10,'Erik',80),(11,'Erlan',70),(12,'Husni',50),(13,'Ayu',80),(14,'Iwan',50),(15,'Rena',90),(16,'Rendi',70),(17,'Reno',45),(18,'Rafa',80),(19,'Mahmud',60),(20,'Nur',70);
+/*!40000 ALTER TABLE `Data` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-25 14:43:29
